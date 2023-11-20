16bdbabe4e29dbdb0f8bdf745f9b0a795b8658eb
ZiyiHuang0708 <148735667+ZiyiHuang0708@users.noreply.github.com>
2023-11-20 17:22:02 +0800

    Add files via upload

--- data_estimation.py
+++ data_estimation.py
@@ -1,7 +1,7 @@
+import argparse
 import numpy as np
-from scipy.stats import pearsonr
+import pandas as pd
 from scipy.spatial.distance import jensenshannon
-import math
 # returns an S1 by S2 matrix P, where S1 is the number sinks and S2 is the number of sources
 # Each row in matrix P sums to 1. Pij is the contribution of source j to sink i
 # estimated_proportions = ...
@@ -18,7 +18,7 @@ def source_jsd_estimation(source_data):
     return jensen_shannon_mean
 
 
-def noise_source_jsd_estimation(noise_data, source_data):
+def jsd_estimation(noise_data, source_data):
     jensen_shannon = []
     for i in range(noise_data.shape[1]):
         noise_pre = noise_data[:, i]
@@ -27,21 +27,6 @@ def noise_source_jsd_estimation(noise_data, source_data):
         jensen_shannon.append(j)
     jensen_shannon_mean = np.mean(jensen_shannon)
     return jensen_shannon_mean
-
-
-# Calculate the squared Pearson correlation (r2) between the estimated and the true mixing proportions per source
-# and average across sources
-def pearson_correlation(estimated_proportions, true_proportions):
-    pearson = []
-    for i in range(estimated_proportions.shape[1]):
-        estimated_data = estimated_proportions[:, i]
-        true_data = true_proportions[:, i]
-        print(estimated_data.shape)
-        print(true_data.shape)
-        r2, p_value = pearsonr(estimated_data, true_data)
-        pearson.append(abs(r2))
-    pearson_mean = np.mean(pearson)
-    return pearson_mean, pearson
 
 
 # Calculate the average Jensen-Shannon divergence of m (based on the pairwise Jensen-Shannon divergence)
@@ -56,9 +41,36 @@ def jensen_shannon_divergence(estimated_proportions, true_proportions):
     return jensen_shannon_mean, jensen_shannon
 
 
-def perf_admm(estimated_proportions, sink_abundance):
-    # pearson, _ = pearson_correlation(sink_abundance, estimated_proportions)
-    jensen_shannon_mean, jensen_shannon = jensen_shannon_divergence(sink_abundance, estimated_proportions)
-    difference = np.sum(abs(sink_abundance - estimated_proportions), axis=1)
-    return jensen_shannon_mean, jensen_shannon, difference
+def perf_admm(estimated_proportions, true_proportions):
+    jensen_shannon_mean, jensen_shannon = jensen_shannon_divergence(estimated_proportions, true_proportions)
+    difference = np.sum(abs(estimated_proportions - true_proportions), axis=1)
+    return jensen_shannon_mean, jensen_shannon, np.mean(difference), difference
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description="""Evaluation of SourceID-NMF Estimated Proportions.""")
+    parser.add_argument('--estimated', '-e', help='input the estimated proportions', default='./estimated_proportions.txt')
+    parser.add_argument('--true', '-t', help='input the true proportions', default='./true_proportions.txt')
+    inputs = parser.parse_args()
+
+    estimated_proportions_path = inputs.estimated
+    true_proportions_path      = inputs.true
+    estimated_proportions = pd.read_csv(estimated_proportions_path, sep=" ", header=0, index_col=0)
+    true_proportions = pd.read_csv(true_proportions_path, sep="	", header=0, index_col=0)
+    row_labels = list(estimated_proportions.index.values)
+    estimated_proportions = np.array(estimated_proportions)
+    true_proportions = np.array(true_proportions)
+
+    perf = []
+    jsd_ave, jsd, diff_ave, diff = perf_admm(estimated_proportions, true_proportions)
+    print("The average JSD between estimated and true proportion is", jsd_ave)
+    print("The average Difference between estimated and true proportion is", diff_ave)
+    perf.append(jsd)
+    perf.append(diff)
+    perf = np.array(perf)
+    nmf_perf_label = ['jsd', 'difference']
+    perf = pd.DataFrame(perf, index=nmf_perf_label, columns=row_labels)
+    perf.to_csv('./proportion_perf.txt', sep=' ')
+
+
 
